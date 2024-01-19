18647667c7c4ba8e6915361f55f6e8bb51c958c3
Jorge Cervantes <jcdev1825@gmail.com>
2024-01-19 16:40:59 -0600

    feat(tags): invoke a lambda

--- src/app/tags/create/app.py
+++ src/app/tags/create/app.py
@@ -1,16 +1,43 @@
 import json
 import logging
+import os
+
+import boto3
+
+
+def request_by_lambda_invoke(lambda_name, payload, invocation_type):
+    lambda_client = boto3.client("lambda", region_name="us-east-1")
+
+    try:
+        response = lambda_client.invoke(
+            FunctionName=lambda_name,
+            InvocationType=invocation_type,  # or 'Event' for asynchronous
+            Payload=json.dumps(
+                {"key": "value"}
+            ),  # data to send to the Lambda function
+        )
+
+        return json.loads(response["Payload"].read())
+    except Exception as e:
+        logging.error(e)
+
+        raise e
 
 
 def lambda_handler(event, context):
+    logging.info(f"event: {event}")
+
     try:
-        logging.info(f"event: {event}")
+        update_task_function = os.getenv("UPDATE_TASK_FUNCTION")
+        res = request_by_lambda_invoke(
+            update_task_function, "", "RequestResponse"
+        )
         data = {
-            "data": None,
+            "data": json.dumps(res),
             "message": "Hello World from CREATE tags lambda function!",
         }
 
         return {"statusCode": 200, "body": json.dumps(data)}
     except Exception as e:
-        logging.error(format(e))
-        return {"statusCode": 500, "body": {"message": json.dumps(e)}}
+        # logging.error(format(e))
+        return {"statusCode": 500, "body": {"message": e}}
